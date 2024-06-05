23f6f40d22ba5cc3ef1f711ccd166a8f2b6603e0
BNB77 <81864906+BNB77@users.noreply.github.com>
2024-06-05 21:51:50 +0300

    Add files via upload

---
+++ 26 unix время макс процессов одновременно в течении недели и в течении скольки секунд.py
@@ -0,0 +1,23 @@
+f = open('26 (1).txt')n = int(f.readline())
+start = 1634515199
+end = start + 604800
+count = 0
+time = [0 for i in range(604801)]
+for i in f:
+    start1, end1 = i.split()
+    if (int(start1) < start) and ((int(end1) > start) or (int(end1) == 0)):
+        count = count + 1
+    if (int(start1) >= start) and (int(start1) <= end):
+        time[int(start1) - start] = time[int(start1) - start] + 1
+    if (int(end1) >= start) and (int(end1) <= end):
+        time[int(end1) - start] = time[int(end1) - start] - 1
+s = 0
+m = 0
+for i in range(1, 604801):
+    count = count + time[i]
+    if count > m:
+        m = count
+        s = 0
+        if count == m:
+            s = s + 1
+            print(m, s)
